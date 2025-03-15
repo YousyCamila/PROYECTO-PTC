bf6803efd8e441242e1d64d85e55af75d02f9a3e
YousyCamila <166034381+YousyCamila@users.noreply.github.com>
2025-03-15 17:57:50 -0500

    feat(front-end): Agregamos nuevas carpetas y un demo del contrato funcional

--- 20.APP Movil/app/src/main/java/com/example/aplicacionptc/Models/Administrador/Contrato/ModelContrato.kt
+++ 20.APP Movil/app/src/main/java/com/example/aplicacionptc/Models/Administrador/Contrato/ModelContrato.kt
@@ -1,6 +1,7 @@
 package com.example.ptc_app.Models.Administrador.Contrato
 
 import com.example.ptc_app.Models.Administrador.Cliente.Clientes
+import com.example.ptc_app.Models.Administrador.Detective.Detectives
 import java.util.Date
 
 class ModelContrato(
@@ -9,17 +10,35 @@ class ModelContrato(
     private var fechaCierre: Date,
     private var clausulas: String,
     private var tarifa: Float,
-    private var estado: Boolean,
+    private var estado: Boolean = true,
     private var cliente: Clientes,
+    private var detective: Detectives,
     private val fechaCreacion: Date = Date()
 ) {
-    // Getters
+    // Getters y Setters
     fun getDescripcionServicio() = descripcionServicio
+    fun setDescripcionServicio(desc: String) { descripcionServicio = desc }
+
     fun getFechaInicio() = fechaInicio
+    fun setFechaInicio(fecha: Date) { fechaInicio = fecha }
+
     fun getFechaCierre() = fechaCierre
+    fun setFechaCierre(fecha: Date) { fechaCierre = fecha }
+
     fun getClausulas() = clausulas
+    fun setClausulas(cl: String) { clausulas = cl }
+
     fun getTarifa() = tarifa
+    fun setTarifa(t: Float) { tarifa = t }
+
     fun getEstado() = estado
+    fun setEstado(est: Boolean) { estado = est }
+
     fun getCliente() = cliente
-    fun getFechaCreacion()= fechaCreacion
+    fun setCliente(cl: Clientes) { cliente = cl }
+
+    fun getDetective() = detective
+    fun setDetective(det: Detectives) { detective = det }
+
+    fun getFechaCreacion() = fechaCreacion
 }
