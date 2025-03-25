00437567dbb2ce36c1c17a1be69ce85bd9530ead
YousyCamila <166034381+YousyCamila@users.noreply.github.com>
2025-03-24 21:35:48 -0500

    feat(Movil):Agregamos la configuracion para funcionamiento apis clientes funcional

--- 20.APP Movil/app/src/main/java/com/example/aplicacionptc/Controllers/Admistrador/Cliente/ControladorCliente.kt
+++ 20.APP Movil/app/src/main/java/com/example/aplicacionptc/Controllers/Admistrador/Cliente/ControladorCliente.kt
@@ -6,19 +6,19 @@ import retrofit2.Call
 import retrofit2.http.*
 
 interface ControladorCliente {
-    @GET("clientes") // Ruta para obtener todos los clientes
+    @GET("clientes")
     fun obtenerClientes(): Call<List<Clientes>>
 
-    @GET("clientes/{id}") // Ruta para buscar cliente por ID
+    @GET("clientes/{id}")
     fun buscarClientePorId(@Path("id") id: String): Call<Clientes>
 
-    @POST("clientes") // Ruta para crear cliente
+    @POST("clientes")
     fun crearCliente(@Body cliente: Clientes): Call<Clientes>
 
-    @PUT("clientes/{id}") // Ruta para actualizar cliente
+    @PUT("clientes/{id}")
     fun actualizarCliente(@Path("id") id: String, @Body cliente: Clientes): Call<Clientes>
 
-    @PATCH("clientes/{id}") // Ruta para desactivar cliente
+    @PATCH("clientes/{id}")
     fun desactivarCliente(@Path("id") id: String): Call<Void>
 }
 
