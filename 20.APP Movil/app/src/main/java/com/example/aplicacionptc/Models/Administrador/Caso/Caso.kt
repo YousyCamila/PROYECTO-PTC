73c86641a0e03d25355b5780a8d9261876000c88
Juan Sebastian Odoñez Sierra <juanseordo2206@outlook.com>
2025-04-04 00:15:49 -0500

    feat : Adicion del modelo Caso

--- 20.APP Movil/app/src/main/java/com/example/aplicacionptc/Models/Administrador/Caso/Caso.kt
+++ 20.APP Movil/app/src/main/java/com/example/aplicacionptc/Models/Administrador/Caso/Caso.kt
@@ -1,11 +1,14 @@
 package com.example.ptc_app.Models.Administrador.Caso
 
-data class Caso(
-    val id: String,
-    val nombreCaso: String,
-    val idCliente: String,
-    val idDetective: String,
-    var activo: Boolean = true,
-    val fechaCreacion: String
+import com.google.gson.annotations.SerializedName
 
+data class Caso(
+    @SerializedName("_id") val id: String? = null,
+    var nombreCaso: String,
+    @SerializedName("idCliente")val idCliente: String,
+    @SerializedName ("idDetective")val idDetective: String,
+    val evidencias: List<String> = emptyList(),
+    val registroCasos: List<String> = emptyList(),
+    val contratos: List<String> = emptyList(),
+    val activo: Boolean = true
 )
