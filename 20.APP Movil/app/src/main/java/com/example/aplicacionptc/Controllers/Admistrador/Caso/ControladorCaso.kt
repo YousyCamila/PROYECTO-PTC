c3d4e15f0ca51c6b37fb534ebd9a31a0c9bbc596
Juan Sebastian Odoñez Sierra <juanseordo2206@outlook.com>
2025-04-04 00:10:14 -0500

    feat : Adicion de ControladorCaso y Retrofit

---
+++ 20.APP Movil/app/src/main/java/com/example/aplicacionptc/Controllers/Admistrador/Caso/ControladorCaso.kt
@@ -0,0 +1,23 @@
+package com.example.aplicacionptc.Controllers.Admistrador.Caso
+
+import com.example.ptc_app.Models.Administrador.Caso.Caso
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ControladorCaso {
+
+    @GET("casos")
+    fun obtenerCasos(): Call<List<Caso>>
+
+    @GET("casos/{id}")
+    fun buscarCasoPorId(@Path("id") id: String): Call<Caso>
+
+    @POST("casos")
+    fun crearCaso(@Body caso: Caso): Call<Caso>
+
+    @PUT("casos/{id}")
+    fun actualizarCaso(@Path("id") id: String, @Body caso: Caso): Call<Caso>
+
+    @PATCH("casos/{id}")
+    fun desactivarCaso(@Path("id") id: String): Call<Void>
+}
