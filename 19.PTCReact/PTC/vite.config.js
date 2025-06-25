18c4120ef807f3af285949d4f9cd55a7a1632a4c
unknown <santiagoprietoavila943@gmail.com>
2025-06-24 19:46:15 -0500

    Se agrego archivo en carpeta public para el manejo de cambio de paginas

--- 19.PTCReact/PTC/vite.config.js
+++ 19.PTCReact/PTC/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // 👈 Asegúrate de que esto esté presente
   plugins: [react()],
 })
